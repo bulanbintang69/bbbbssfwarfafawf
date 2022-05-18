3f5f86b0b4d20a6665c9549ad91b38f29a303965
INCREDIBLE POSISI <87313549+bulanbintang69@users.noreply.github.com>
2022-05-18 15:26:00 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -113,7 +113,6 @@ async def start_command(client: Bot, message: Message):
             except BaseException:
                 pass
     else:
-        out = start_button(client)
         await message.reply_text(
             text=START_MSG.format(
                 first=message.from_user.first_name,
